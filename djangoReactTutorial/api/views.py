072b4226dbb98590345dd9088d09480bdf18bb06
Chaudhary Umair <umairraza95300@gmail.com>
2025-05-05 18:29:08 +0500

    first commit

---
+++ djangoReactTutorial/api/views.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user   # to get the user calling it
+        return Note.objects.filter(author=user) # getting all the note written by this user
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user) # adding the author here
+        else: 
+            print(serializer.errors)    
+         
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = Note
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user   # to get the user calling it
+        return Note.objects.filter(author=user) # getting all the note written by this user
+
+    
+
+
+# Create your models here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny] 
+    
