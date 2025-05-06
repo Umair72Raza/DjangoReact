0c0c25ceea6b8d3fa50b74bb8c745c0cc3761425
Chaudhary Umair <umairraza95300@gmail.com>
2025-05-06 11:38:14 +0500

    env changes try

--- djangoReactTutorial/backend/settings.py
+++ djangoReactTutorial/backend/settings.py
@@ -97,11 +97,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+print(os.getenv("DB_USER"))
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PWD"),
